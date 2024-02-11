2551b9007072566011125bc47f01869ed5353903
Foysalrabby <81823837+Foysalrabby@users.noreply.github.com>
2024-02-11 19:53:00 +0600

    setstate methhod reflect buld count

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
@@ -10,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
@@ -33,19 +35,32 @@ class MyHomePage extends StatefulWidget{
 
 // implement StatefulWidget of myhomepageclass in mystate class
 class Mystate extends State<MyHomePage>{
+  var count=0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
        appBar:AppBar(
          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-         title: Text("this is stateful app",),
-
-
-
+         title: Text("this is stateful app",)
        ),
 
       body: Center(
-        child: Text("hello"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("cunter: $count"),
+            ElevatedButton(onPressed: (){
+
+             count++;
+             print(count);
+             setState(() {
+               
+             });
+            }, child: Text("count"))
+          ],
+        ),
+
+
       ),
 
 
