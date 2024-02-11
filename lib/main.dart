66a554cf78421724a4dc87c863b52ff2c2b0e217
Foysalrabby <81823837+Foysalrabby@users.noreply.github.com>
2024-02-11 19:42:17 +0600

    customaize statefulwidget theme use backgruond color in theme

--- lib/main.dart
+++ lib/main.dart
@@ -15,53 +15,41 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
+ //create manual statefulwidget
+class MyHomePage extends StatefulWidget{
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<StatefulWidget> createState() {
+  return Mystate();
+  }
+  // State<StatefulWidget> createState() => Mystate();  //USE  {} return => ai vabe write kora jai
+
+
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-
+// implement StatefulWidget of myhomepageclass in mystate class
+class Mystate extends State<MyHomePage>{
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      appBar: AppBar(
+       appBar:AppBar(
+         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+         title: Text("this is stateful app",),
 
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
 
-        title: Text(widget.title),
+
+       ),
+
+      body: Center(
+        child: Text("hello"),
       ),
-      body: Container(
-        width: double.infinity,
-          height: double.infinity,   //to take all the space
-          color: Colors.amber,
-          child: Stack(
-            children: [
-              Positioned(
-                bottom: 20,
-                  right: 20,
-                  child: Container(
-                width: 70,
-                height: 70,
-                color: Colors.blue,
-              ))
 
-            ],
-          )
-          
-
-      ),
 
     );
   }
+
 }
