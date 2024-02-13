875df9aec5a7523fe366610dfcf3a7ce34927569
Foysalrabby <81823837+Foysalrabby@users.noreply.github.com>
2024-02-13 19:14:46 +0600

    to go one page to another Introduct to homepage

---
+++ lib/introduc.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:myappflater/main.dart';
+
+class Introduct extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("THIS is Intropage"),
+       backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            width: 200,
+            height: 400,
+            color: Colors.redAccent,
+            child: Text("this is new introduction page"),
+          ),
+          ElevatedButton(onPressed: (){
+
+            Navigator.push(context,MaterialPageRoute(builder: (context){return MyHomePage();}));
+            //to go one page  to another
+
+          }, child: Text("go next page"))
+        ],
+      ),
+
+    );
+  }
+  
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:myappflater/introduc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: MyHomePage(),
+      home: Introduct(),
     );
   }
 }
@@ -35,9 +36,7 @@ class MyHomePage extends StatefulWidget{
 
 // implement StatefulWidget of myhomepageclass in mystate class
 class Mystate extends State<MyHomePage>{
-  var textcontroller1=TextEditingController();
-  var textcontroller2=TextEditingController();
-  var result=0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,79 +45,7 @@ class Mystate extends State<MyHomePage>{
          title: Text("this is stateful app",)
        ),
 
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextField(
-                controller: textcontroller1,
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Enter first number',
-                ),
-              ),
-              SizedBox(height: 10,),
-              TextField(
-                controller: textcontroller2,
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Enter second number',
-                ),
-
-              ),
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Padding(padding: EdgeInsets.only(top: 60),),
-                  ElevatedButton(onPressed: (){
-                    var num1=int.parse(textcontroller1.text.toString());
-                    var num2=int.parse(textcontroller2.text.toString());
-                    result=num1+num2;
-                    print(result);
-                    setState(() {
-
-                    });
-                  }, child: Text("add")),
-                  ElevatedButton(onPressed: (){
-                    var num1=int.parse(textcontroller1.text.toString());
-                    var num2=int.parse(textcontroller2.text.toString());
-                    result=num1-num2;
-                    print(result);
-
-                    setState(() {
-                    });
-                  }, child: Text("sub")),
-                  ElevatedButton(onPressed: (){
-                    var num1=int.parse(textcontroller1.text.toString());
-                    var num2=int.parse(textcontroller2.text.toString());
-                    result=num1*num2;
-                    print(result);
-                    setState(() {
-                    });
-                  }, child: Text("mul")),
-                  ElevatedButton(onPressed: (){
-                    var num1=int.parse(textcontroller1.text.toString());
-                    var num2=int.parse(textcontroller2.text.toString());
-                    double result2=num1/num2;
-                    print(result2);
-                    setState(() {
-                    });
-                  }, child: Text("div"))
-                ],
-              ),
-              Text("Result : $result", style: TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),)
-
-            ],
-          ),
-
-
-        ),
-      ),
+      body:Text("hello"),
 
 
     );
