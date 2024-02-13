80a7619fb475e4737b8f82afad8e23f72e2f2e10
Foysalrabby <81823837+Foysalrabby@users.noreply.github.com>
2024-02-13 20:02:08 +0600

    sorry need initState

--- lib/slpashscreen.dart
+++ lib/slpashscreen.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:myappflater/main.dart';
 
 class Slpashsceen extends StatefulWidget{
   @override
@@ -10,6 +13,17 @@ class Slpashsceen extends StatefulWidget{
 }
 class Slpahstate extends State<StatefulWidget>{
 
+   @override
+   void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(Duration (seconds: 4,),(){
+      Navigator.pushReplacement(context,MaterialPageRoute(builder: (context){
+        return MyHomePage();
+      }));
+    }
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
