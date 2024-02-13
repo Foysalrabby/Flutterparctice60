deb0e5cf9d454bb30bd60cdb340400aadfadc6d5
Foysalrabby <81823837+Foysalrabby@users.noreply.github.com>
2024-02-13 19:47:44 +0600

    to take timmer option need StatefulWidget and  Slpahstate class  for Timer and intialiage methodi

--- lib/slpashscreen.dart
+++ lib/slpashscreen.dart
@@ -1,18 +1,42 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class Slpashsceen extends StatelessWidget{
+class Slpashsceen extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+   return  Slpahstate();
+  }
+
+}
+class Slpahstate extends State<StatefulWidget>{
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       body: Container(
-         color: Colors.blue,
-         child: Center(
-           child: Text("Muslim",style: TextStyle(color: Colors.white,fontWeight: FontWeight.w400),),
+        body: Container(
+          color: Colors.blue,
+          child: Center(
+            child: Text("Muslim",style: TextStyle(color: Colors.white,fontWeight: FontWeight.w600,fontSize: 30),),
          ),
-       ),
-
+        )
     );
   }
 
 }
+
+
+// class Slpashsceen extends StatelessWidget{
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//        body: Container(
+//          color: Colors.blue,
+//          child: Center(
+//            child: Text("Muslim",style: TextStyle(color: Colors.white,fontWeight: FontWeight.w600,fontSize: 30),),
+//          ),
+//        ),
+//
+//     );
+//   }
+//
+// }
